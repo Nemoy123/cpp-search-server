7768eeabdf22255c52292d7402eed872cf71ae0e
Nemoy123 <lipkin1104@gmail.com>
2023-07-24 20:21:44 +0300

    sprint6 rev3

--- search-server/remove_duplicates.cpp
+++ search-server/remove_duplicates.cpp
@@ -1,46 +1,35 @@
 #include "remove_duplicates.h"
 
 
-std::set <int> TestDuplicates (const int document_id, const SearchServer& search_server) {
-    const std::map <std::string, double>& find_words = search_server.GetWordFrequencies(document_id);
-    std::set <std::string> test_set = {};
-    std::set <int> result = {};
-    std::set <std::string> set_from_test_id_map;
-    for (const auto& [word, number] : find_words){
-        test_set.insert (word);
-    }
-    for (const int testing_id : search_server) {
-        if (testing_id == document_id || result.count(testing_id) > 0) {continue;}
-        set_from_test_id_map.clear();
+void RemoveDuplicates (SearchServer& search_server) {
+    
+    std::set <std::string> set_from_test_id_map = {};
+    std::set <int> duplicates={};
+    std::map <std::set<std::string>, int> nabor_slov_vseh_documents; // int id, set слова
+    for (int testing_id : search_server) {
         
         const std::map <std::string, double>& test_id_map = search_server.GetWordFrequencies(testing_id);
         
         for (const auto& [word, number] : test_id_map) {
             set_from_test_id_map.insert (word);
         }
-        if (test_set == set_from_test_id_map) {
-            result.insert (testing_id);
-        }
-    }
-    return result;
-
-}
-
-void RemoveDuplicates(SearchServer& search_server) {
-    std::set <int> duplicates={};
-    
-    for (const int document_id : search_server) {
-        if (duplicates.count(document_id) > 0 ) {continue;}
-        //const std::map<std::string, double>& SearchServer::GetWordFrequencies(int document_id) const {
-
-        std::set <int> temp = TestDuplicates(document_id, search_server);
-        duplicates.insert (temp.begin(), temp.end());
-    }
-    for (auto duplicat : duplicates) {
         
-        std::cout << "Found duplicate document id "s<< duplicat << std::endl;
-        search_server.RemoveDocument (duplicat);
+        if (nabor_slov_vseh_documents.count(set_from_test_id_map) > 0) {
+            duplicates.insert (testing_id);
+        }
+        else {
+            nabor_slov_vseh_documents.insert ({set_from_test_id_map, testing_id});
+        }
+        set_from_test_id_map.clear();
+         
     }
+    for (auto duplicate : duplicates) {
+        
+        std::cout << "Found duplicate document id "s << duplicate << std::endl;
+        search_server.RemoveDocument (duplicate);
+    }
+
 }
+
 
 
--- search-server/remove_duplicates.h
+++ search-server/remove_duplicates.h
@@ -2,5 +2,5 @@
 #include <iostream>
 #include "search_server.h"
 
-void RemoveDuplicates(SearchServer& search_server);
-std::set <int> TestDuplicates (const int document_id, const SearchServer& search_server);
+
+void RemoveDuplicates (SearchServer& search_server);
